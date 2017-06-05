31b81e05edee08140020852f58b245d880975e92
Paul Joiner <pauluntakjoiner@gmail.com>
2017-06-05 18:48:09 -0500

    first commit with firebase and maps implemented

--- js/main.js
+++ js/main.js
@@ -1,19 +1,55 @@
-$('#reservation').on('click', function (e){
-	e.preventDefault();
-	$('#form').show();
-});
-$('#confirm').on('click', function (e) {
-	e.preventDefault();
-	var userInput = {name: $('#name').val(), day: $('#day').val()};
-	var source = $('#tableItem').html();
-	var template = Handlebars.compile(source);
-	var newTableItemHTML = template(userInput);
-	$('#table tbody').append(newTableItemHTML);
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBQ8ey4bfUhXssLcjawNwZx-WYwQMICb4I",
+    authDomain: "reservation-site-15aee.firebaseapp.com",
+    databaseURL: "https://reservation-site-15aee.firebaseio.com",
+    projectId: "reservation-site-15aee",
+    storageBucket: "reservation-site-15aee.appspot.com",
+    messagingSenderId: "94098594371"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  var reservationData = {};
+$('.reservation-day li').on('click', function (e) {
+  reservationData.day = $(this).text();
+  console.log(reservationData.day);
 });
 
+$('.reservation-button').on('click', function (e){
+    e.preventDefault();
+    reservationData.name = $('.reservation-name').val();
+    var reservationsReference = database.ref('reservations');
+    reservationsReference.push(reservationData);
+})
+
 var initMap = function () {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('mapDiv'), {
     center: {lat: 40.8054491, lng: -73.9654415},
     zoom: 8
   });
-}
+};
+
+var getReservations = function () {
+    database.ref('reservations').on('value', function (results) {
+      var allReservations = results.val();
+      $('.reservations-list').empty();
+      
+      for (var reservation in allReservations) {
+        var context = {
+        name: allReservations[reservation].name,
+        day: allReservations[reservation].day,
+        reservationId: reservation
+        };
+      var source = $("#reservation-template").html();
+
+      var template = Handlebars.compile(source);
+
+      var reservationListItem = template(context);
+
+      $('.reservation-list').append(reservationListItem);
+      }
+  });
+};
+
+getReservations();
+
